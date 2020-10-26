05bd8c6198d613e969cae5ab8525e56441037c0e
n-kazami <n-kazami@cmind-co.jp>
2020-10-26 17:59:36 +0900

    投稿データを保存しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
