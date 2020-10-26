cccfd32ee45f564abba5ebe11ba35708e2d56132
n-kazami <n-kazami@cmind-co.jp>
2020-10-26 18:25:55 +0900

    投稿したニュース一覧を表示しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,8 +14,19 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        //  // Varidationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        unset($form['_token']);
+
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
@@ -27,5 +38,18 @@ class ProfileController extends Controller
     public function update()
     {
         return redirect('admin/profile/edit');
+    }
+
+    public function index(Request $request)
+    {
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
 }
